fa80bb17e239e2c0b9d1bd5c24b4e9a91d603259
Tobias Jern <tobias.jern@aalto.fi>
2021-11-19 21:18:01 +0200

    Modify add event to accept POST

--- backend/app.py
+++ backend/app.py
@@ -30,19 +30,31 @@ def events(event_type):
         return 'No events found'
 
 
-@app.route('/add_event')
+@app.route('/add_event', methods=['GET', 'POST'])
 def add():
-    if request.method == 'GET':
+    if request.method == 'POST':
         db_actions = db_connect()
         add_event = db_actions['add']
-        event = request.args.get('event', '')
-        user = request.args.get('user', '')
+        event = request.form['event']
+        user = request.form['user']
         if user and event:
             add_event(event, user)
-    return 'ok'
+            return 'ok'
+        else:
+            return 'Wrong data.\nRequired: user, event'
+    elif request.method == 'GET':
+        return jsonify({
+            'message': 'Post a new event',
+            'fields': [
+                { 'name': 'user', 'type': 'string' },
+                { 'name': 'event', 'type': 'string' },
+            ],
+        })
+    else:
+        return f'Method "{request.method}" not supported'
 
 
 if __name__ == '__main__':
     with app.app_context():
         db_connect()['setup']()
-    app.run(port=80, host="0.0.0.0", debug=True)
+    app.run(port=5000, host="0.0.0.0", debug=True)
