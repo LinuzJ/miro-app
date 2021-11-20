4cf1938f65d31a97276ab35b75899a20c50eb929
Tobias Jern <tobias.jern@aalto.fi>
2021-11-20 18:05:41 +0200

    Try to access miro data

--- frontend/dashboard.js
+++ frontend/dashboard.js
@@ -1,28 +1,30 @@
-       function openTab(selected) {
-         const tabIndices = { '.user-stats': 0, '.productivity': 1, '.misc-stats': 2 };
-         ['.user-stats', '.productivity', '.misc-stats'].forEach(tab => {
-            if (selected === tab) {
-                   document.querySelector(tab).style = 'display: flex;'
-                   document.querySelector(`[tabindex = "${tabIndices[tab]}"`).classList.add('tab-active')
-                 } else {
-                   document.querySelector(tab).style = 'display: none;'
-                   document.querySelector(`[tabindex = "${tabIndices[tab]}"`).classList.remove('tab-active')
-                     };
-         });
-       }
-        
-     async function getActivity() {
-            const boardId = 'o9J_lhl9hPw=';
-            const resp = await fetch('https://hittatilltf.com/stats/productivity');
-            productivityData = await resp.json();
-            const list = document.querySelector('.productivity-list');
-            Object.entries(productivityData[boardId]).forEach(([user, productivityScore]) => {
-              const li = document.createElement('li');
-              li.appendChild(document.createTextNode(`${user} --- ${productivityScore.toFixed(3)}`));
-              list.appendChild(li);
-            });
+let board = null;
 
-          }
+function openTab(selected) {
+  const tabIndices = { '.user-stats': 0, '.productivity': 1, '.misc-stats': 2 };
+  ['.user-stats', '.productivity', '.misc-stats'].forEach(tab => {
+    if (selected === tab) {
+      document.querySelector(tab).style = 'display: flex;'
+      document.querySelector(`[tabindex = "${tabIndices[tab]}"`).classList.add('tab-active')
+    } else {
+      document.querySelector(tab).style = 'display: none;'
+      document.querySelector(`[tabindex = "${tabIndices[tab]}"`).classList.remove('tab-active')
+    };
+  });
+}
+
+async function getActivity() {
+  const boardId = 'o9J_lhl9hPw=';
+  const resp = await fetch('https://hittatilltf.com/stats/productivity');
+  productivityData = await resp.json();
+  const list = document.querySelector('.productivity-list');
+  Object.entries(productivityData[boardId]).forEach(([user, productivityScore]) => {
+    const li = document.createElement('li');
+    li.appendChild(document.createTextNode(`${user} --- ${productivityScore.toFixed(3)}`));
+    list.appendChild(li);
+  });
+
+}
 getActivity();
 
 async function getInsights() {
@@ -41,3 +43,35 @@ async function getInsights() {
 };
 
 getInsights();
+
+function setInfo() {
+  if (board) {
+    const p = document.querySelector('.board-info');
+    p.appendChild(document.createTextNode(`Board: ${board.id}\nCreated at: ${baoprd.createdAt}\nLast modified: ${board.updatedAt}\nOwner: ${board.owner.name}`));
+  } else {
+    setTimeout(setInfo, 100000);
+  }
+}
+
+setInfo();
+
+async function requestAuthorization() {
+    let isAuthorized = await miro.isAuthorized()
+    while (!isAuthorized) {
+        try {
+            await  miro.requestAuthorization();
+        } catch (e) {
+            await new Promise(r => setTimeout(r, 200));
+        }
+        isAuthorized = await miro.isAuthorized()
+    }
+}
+
+miro.onReady(async () => {
+    console.log('Snart e de ylonz!!');
+    await requestAuthorization();
+    initialize();
+    const boardInfo = await miro.board.info.get();
+    board = boardInfo;
+    setInfo();
+});
