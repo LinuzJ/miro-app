cf7a68147e9a78d127da8eb53011b620833804b8
Anselmi Jokinen <63022882+ansjokinen@users.noreply.github.com>
2021-11-20 12:55:39 +0200

    Fix choise panel path

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -101,7 +101,7 @@ miro.onReady(() => {
             title: 'analytics toolkit',
             svgIcon: '<circle cx="12" cy="12" r="9" fill="none" fill-rule="evenodd" stroke="currentColor" stroke-width="2"></circle>',
             onClick: () => {
-                miro.board.openModal('choise.html');
+                miro.board.openModal('frontend/choise.html');
             },
           },
         },
