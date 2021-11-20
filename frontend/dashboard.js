b3feb4bb90195cf3b9dd659525014c181775410e
Tobias Jern <tobias.jern@aalto.fi>
2021-11-20 19:37:06 +0200

    Add user activity chart

--- frontend/dashboard.js
+++ frontend/dashboard.js
@@ -50,11 +50,72 @@ function setInfo() {
   }
 }
 
-miro.onReady(async () => {
+async function showUserChart() {
+  const scan = (values, fn, initial) => values.reduce((total, n) => [...total, fn(total[total.length - 1], n)], [initial])
+  const resp = await fetch('https://hittatilltf.com/events')
+  const events = await resp.json()
+  labels = []
+  join_leave_events = events.filter((event) => ['USER_JOINED', 'USER_LEFT'].includes(event[1]));
+  console.log(scan(join_leave_events, (total, n) => {
+    if (n[1] === 'USER_JOINED') {
+      labels.push(n[3])
+      return { x: n[5], y: total.y + 1 }
+    } else {
+      labels.push(n[3])
+      return { x: n[5], y: total.y - 1 }
+    }
+  }, { x: join_leave_events[0][5], y: 0 }));
+  const data = {
+    datasets: [{
+      label: 'User online activity',
+      labels,
+      data: scan(join_leave_events, (total, n) => {
+        if (n[1] === 'USER_JOINED') {
+          return { x: n[5], y: total.y + 1, label: n[3] }
+        } else {
+          return { x: n[5], y: total.y - 1, label: n[3] }
+        }
+      }, { x: join_leave_events[0][5], y: 0 }),
+      backgroundColor: 'rgb(255, 99, 132)',
+      borderColor: 'rgb(255, 99, 132)',
+    }]
+  };
+  const config = {
+    type: 'scatter',
+    data: data,
+    options: {
+      responsive: true,
+      scales: {
+        x: {
+          type: 'timeseries',
+          position: 'bottom'
+        }
+      },
+      plugins: {
+        tooltip: {
+          callbacks: {
+            label: function(context) {
+              return labels[context.dataIndex];
+            }
+          }
+        }
+      }
+
+    }
+  };
+  const myChart = new Chart(
+    document.querySelector('#userChart'),
+    config
+  );
+}
+showUserChart();
+
+/* miro.onReady(async () => {
     console.log('Snart e de ylonz!!');
     const boardInfo = await miro.board.info.get();
     board = boardInfo;
     getInsights();
     setInfo();
     getActivity(board.id);
-});
+    showUserChart();
+}); */
